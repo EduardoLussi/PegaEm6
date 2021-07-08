4728a75c86919f09a858ba078f141ad5a582b109
EduardoLussi <eduardolussi@gmail.com>
2021-07-08 11:39:50 -0300

    Tela de Partida pronta

--- Implementação/Views/Components/Fileira.py
+++ Implementação/Views/Components/Fileira.py
@@ -3,13 +3,15 @@ from os import path
 
 
 class Fileira(Frame):
-    def __init__(self, master):
+    def __init__(self, master, qtCartas=5):
         Frame.__init__(self, master=master)
         pathName = path.abspath(path.dirname('')).replace("\\", "/")
-        self.cartas = [PhotoImage(file=f"{pathName}/img/cartas/9.png")] * 5
+        self.cartas = [PhotoImage(file=f"{pathName}/img/cartas/9.png")] * qtCartas
         self.atualizarCartas()
 
     def atualizarCartas(self):
         for i, carta in enumerate(self.cartas):
-            lblCarta = Label(self, image=carta)
+            lblCarta = Label(self, image=carta, bg='white', cursor="hand2")
+            lblCarta.bind("<Enter>", lblCarta.configure(fg="#ead215"))
+            lblCarta.bind("<Leave>", lblCarta.configure(fg="white"))
             lblCarta.grid(row=0, column=i)
--- Implementação/Views/Partida.py
+++ Implementação/Views/Partida.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from Components.Mesa import Mesa
+from Components.Fileira import Fileira
 
 
 class Partida(Frame):
@@ -18,10 +19,52 @@ class Partida(Frame):
         self.master.title("Mesa")
 
         self.frMesa = Mesa(self)
-        self.frMesa.pack()
+        self.frMesa.place(relx=0.16, rely=0.34, anchor=CENTER)
+
+        self.frPlacar = Frame(self, bg='white')
+
+        self.lblVez = Label(self.frPlacar, text="Vez de jogador 1")
+        self.lblVez.configure(font=("Century Gothic", 20),
+                                  bg="white",
+                                  fg="#0e6fb6")
+        self.lblVez.pack(anchor=E)
+
+        self.lblProxVez = Label(self.frPlacar, text="Próximo a jogar será Jogador 2")
+        self.lblProxVez.configure(font=("Century Gothic", 20),
+                                  bg="white",
+                                  fg="#0e6fb6")
+        self.lblProxVez.pack(anchor=E)
+
+        self.frLinha = Frame(self.frPlacar, background='#ead215', height=3, width=500)
+        self.frLinha.pack(anchor=E, pady=20)
+
+        self.jogadores = [12, 31, 34]
+        for i, jogador in enumerate(self.jogadores):
+            lblJogador = Label(self.frPlacar, text=f"Jogador {i+1}: {self.jogadores[i]} pontos")
+            lblJogador.configure(font=("Century Gothic", 15),
+                                  bg="white",
+                                  fg="#0e6fb6")
+            lblJogador.pack(anchor=E)
+
+        self.frPlacar.place(relx=0.85, rely=0.13, anchor=CENTER)
+
+        self.frMao = Fileira(self, 10)
+        self.frMao.place(relx=0.5, rely=0.82, anchor=CENTER)
+
+        self.lblMessage = Label(self, text="Escolha sua carta")
+        self.lblMessage.configure(font=("Century Gothic", 22),
+                                  bg="white",
+                                  fg="#0e6fb6")
+        self.lblMessage.place(relx=0.5, rely=0.95, anchor=CENTER)
+
+        self.imgRestart = PhotoImage(file="img/refresh.png")
+        self.lblRestart = Label(self, image=self.imgRestart, bg='white', cursor="hand2")
+        self.lblRestart.place(relx=0.04, rely=0.93, anchor=CENTER)
 
         # self.master.withdraw() # Esconde root da tela atual
         # self.abrirTelaIniciarJogada()  # Chama a tela de iniciar jogada
+
+        self.pack()
 
     # def abrirTelaIniciarJogada(self):
     # root = Toplevel()  # Cria root da tela de iniciar jogada
